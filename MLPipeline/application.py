e1b763b617afd0f5656296ef363b04dbe5dcd839
kulraghav <kulraghav@gmail.com>
2018-12-29 20:34:35 +0800

    added: Dockerfile

--- MLPipeline/application.py
+++ MLPipeline/application.py
@@ -66,4 +66,4 @@ def retrain():
             raise Exception("Training was unsuccessful!")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
--- MLPipeline/model_loader.py
+++ MLPipeline/model_loader.py
@@ -1,7 +1,12 @@
 import os
 from sklearn.externals import joblib
 
+model_dir = "../Models/Spam/"
+
+
 def get_latest_model_path(model_dir):
+    return "model.joblib_2018_December_29" # temporary fix to check docker deployment
+
     filenames = []
     for root, dirs, files in os.walk(model_dir):  
         for filename in files:
@@ -15,7 +20,7 @@ def get_latest_model_path(model_dir):
     return model_dir+filenames[0]
     
 
-def load_model(path_to_model_dir="../Models/Spam/", path_to_model=""):
+def load_model(path_to_model_dir=model_dir, path_to_model=""):
     if not path_to_model:
         path = get_latest_model_path(path_to_model_dir)
     else:
