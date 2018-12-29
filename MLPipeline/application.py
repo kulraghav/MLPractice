b011df33fa505c0c6301e727d741c4353820389c
kulraghav <kulraghav@gmail.com>
2018-12-29 16:50:21 +0800

    added: retrain API

--- MLPipeline/application.py
+++ MLPipeline/application.py
@@ -24,8 +24,6 @@
 """
 
 from flask import Flask, request, jsonify
-from sklearn.externals import joblib
-import pandas as pd
 
 app = Flask(__name__)
 
@@ -33,29 +31,7 @@ app = Flask(__name__)
 def welcome_message():
     return "Welcome to the spam-filter web service!"
 
-import os
-def get_latest_model_path(model_dir):
-    filenames = []
-    for root, dirs, files in os.walk(model_dir):  
-        for filename in files:
-            filenames.append(filename)
-            
-    filenames.sort(key=lambda x: x.split('joblib')[-1], reverse=True)
-
-    if not filenames:
-        raise Exception("Model not found")
-    
-    return model_dir+filenames[0]
-    
-
-def load_model(path_to_model_dir="../Models/Spam/", path_to_model=""):
-    if not path_to_model:
-        path = get_latest_model_path(path_to_model_dir)
-    else:
-        path = path_to_model
-    model = joblib.load(path)
-    return model
-    
+from model_loader import load_model
 model = load_model()    
 
 @app.route("/predict", methods=['POST'], strict_slashes=False)
@@ -72,6 +48,21 @@ def predict():
             return jsonify("Please enter a valid input: {'text': 'Your text goes here'}")
 
         return jsonify({'prediction': prediction, 'spam_proba': spam_proba})
-    
+
+from data_loader import load_data    
+from trainer import train
+
+@app.route("/retrain", methods=['GET'], strict_slashes=False)    
+def retrain():
+    if request.method == 'GET':
+        try:
+            X, y = load_data("../Data/spam.csv")
+            print("loaded data")
+            
+            result = train(X, y, save=True)
+            return jsonify(result)
+        except:
+            raise Exception("Training was unsuccessful!")
+
 if __name__ == '__main__':
     app.run(debug=True)
