5fe6f386313305d04068c9e53fe722c346819980
kulraghav <kulraghav@gmail.com>
2019-01-11 22:00:36 +0800

    completed: create tables

--- SearchEngine/indexer.py
+++ SearchEngine/indexer.py
@@ -5,12 +5,23 @@
 
 import sqlite3
 
-conn = sqlite3.connect('test.db')
+con = sqlite3.connect('test.db')
 
 print("Opened database successfully.")
 
 def create_tables():
-   pass
+   con.execute('create table urllist(url)')
+   con.execute('create table wordlist(word)')
+   con.execute('create table wordlocation(urlid,wordid,location)')
+   con.execute('create table link(fromid integer,toid integer)')
+   con.execute('create table linkwords(wordid,linkid)')
+
+   con.execute('create index on wordlist(word)')
+   con.execute('create index on urllist(url)')
+   con.execute('create index on wordlocation(wordid)')
+   con.execute('create index urltoidx on link(toid)')
+   con.execute('create index urlfromidx on link(fromid)')
+   dbcommit( )            
 
 def get_text(url):
    pass
