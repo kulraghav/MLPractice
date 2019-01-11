75f8919b349bd16e7e5f02bd432e71586fbb01a3
kulraghav <kulraghav@gmail.com>
2019-01-11 21:42:32 +0800

    generate urls

--- SearchEngine/crawler.py
+++ SearchEngine/crawler.py
@@ -1,9 +1,15 @@
 
 import requests
 from bs4 import BeautifulSoup
+from indexer import create_index
 
 def is_valid(link):
-    return True # test: is_valid function takes a lot of time
+    return True
+
+    """
+        is_valid function takes forever for 1000 links
+        Skipping it temporarily
+    """
 
     try:
         requests.get(link)
@@ -35,9 +41,19 @@ def crawl_and_index(base_urls, depth=2):
             links = get_valid_links(base_url)
             for link in links:
                 if not link in indexed_urls:
+                    create_index(link)
                     indexed_urls.add(link)
                     
-                    
-    return indexed_urls         
+    return indexed_urls
+
+
+def generate_urls(base_urls, depth=2):
+    urls = set(base_urls)
+    for i in range(depth):
+        for base_url in base_urls:
+            links = get_valid_links(base_url)
+            urls = urls.union(links)
+
+    return urls 
             
         
